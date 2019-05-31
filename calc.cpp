42ade3ac76751e9c7db78baa39aa9ef8f0e8d69c
avral <qweasd_96@mail.ru>
2019-05-31 10:34:32 +0300

    retab

--- calc.cpp
+++ calc.cpp
@@ -4,33 +4,33 @@ using namespace eosio;
 
 
 class [[eosio::contract]] calc : public eosio::contract {
-	using contract::contract;
+   using contract::contract;
 
-	public:
+   public:
       calc(name receiver, name code, datastream<const char*> ds)
          :contract(receiver, code, ds),
          _results(receiver, receiver.value){};
 
       [[eosio::action]]
-		void calculate(const uint64_t x) {
+      void calculate(const uint64_t x) {
          uint64_t result = x * 8;
          
-			_results.emplace(_self, [&]( auto& r ) {
-				r.id = _results.available_primary_key();
-				r.result = result;
-			});
+         _results.emplace(_self, [&]( auto& r ) {
+            r.id = _results.available_primary_key();
+            r.result = result;
+         });
 
          print("Result: " + std::to_string(result));
       };
-	private:
-		struct [[eosio::contract]] result {
-			uint64_t id;
+   private:
+      struct [[eosio::contract]] result {
+         uint64_t id;
          uint64_t result;
 
          uint64_t primary_key() const { return id; }
-		};
-	
-		typedef eosio::multi_index<"results"_n, result> results;
+      };
+   
+      typedef eosio::multi_index<"results"_n, result> results;
 
       results _results;
 };
